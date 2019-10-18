7467e15f4e3d7c02a215272315570f20ffb33746
SURYAPRAKASH S <suryaprakash0831@gmail.com>
2019-10-18 19:32:52 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -6,7 +6,7 @@ qr = q.QRCode(
 	border=5
 )
 
-data = input("enter the data or the url which has to be encoded in qr code:")
+data = input(r"enter the data or the url which has to be encoded in qr code:")
 qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill='black', back_color='white') #you can change the background and fill color 
